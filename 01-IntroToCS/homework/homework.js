2e17051d011f106f827b6b94f7e931510c0cce36
arielbg00 <ariel-bravo96@hotmail.com>
2022-07-19 10:23:25 -0300

    first commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -6,10 +6,9 @@ function BinarioADecimal(num) {
   let numDecimal = parseInt(num, 2);
   return numDecimal;
   */
-  var numToString = num.toString();
   var numDecimal = 0;
   var pos = 0;
-  for (var i = numToString.length - 1; i >= 0; i--) {
+  for (var i = num.length - 1; i >= 0; i--) {
     numDecimal += parseInt(num[i]) * (2 ** pos);
     pos++;
   }
@@ -22,13 +21,13 @@ function DecimalABinario(num) {
   let numBinario = num.toString(2);
   return numBinario;
   */
-  var numBinario = [];
-  var decimal = num;
-  while (decimal > 0) {
-    numBinario.push(decimal % 2);
-    decimal = Math.floor(decimal / 2);
+  var numBinario = "";
+  while (num > 0) {
+    //numBinario += decimal % 2; 
+    numBinario = num % 2 + numBinario;
+    num = Math.floor(num / 2);
   }
-  return numBinario.reverse().join("");
+  return numBinario;
 }
 
 
